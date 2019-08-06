fdc4d4d2c49582935113ce3c54c186b47afb591d
adrielhigor <adrielhigor@gmail.com>
2019-08-06 03:47:41 -0300

    api_join token update

---
+++ hub/app/ctrl_p/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'app.ctrl_p.apps.CtrlPConfig'
--- hub/app/ctrl_p/serializers.py
+++ hub/app/ctrl_p/serializers.py
@@ -6,7 +6,7 @@ class FileSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = models.File
-        fields = ('pk', 'user', 'name', 'copy', 'status')
+        fields = ('pk', 'user','name', 'copy', 'status')
 
 # class CreateFileSerializer(serializers.ModelSerializer):
 #     pk = serializers.UUIDField(read_only=True)
