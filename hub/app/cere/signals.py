f51a2be1de0a1f3671929d38a8c10f5c10c6a2b6
AdrielHigor <adrielhigor@gmail.com>
2019-09-03 23:38:13 -0300

    CERE task : update

--- hub/app/cere/signals.py
+++ hub/app/cere/signals.py
@@ -1,7 +1,13 @@
 from app.cere import models, tasks
 from django.db.models.signals import post_save
 
-def create_company_post_save(sender, instance,created, **kwargs):
-    tasks.agreement()
+def create_company_post_save(sender, instance, created, **kwargs):
+    tasks.agreement(instance)
+
+def create_document_opinion_post_save(sender, instance, created, **kwargs):
+    tasks.document_opinion(instance)
+
+
 
 post_save.connect(create_company_post_save, sender=models.Company)
+post_save.connect(create_document_opinion_post_save, sender=models.Document_opinion)
--- hub/app/ctrl_p/tasks.py
+++ hub/app/ctrl_p/tasks.py
@@ -2,6 +2,7 @@ from huey.contrib.djhuey import task
 from app.ctrl_p import models
 from django.core.mail import EmailMessage
 
+
 @task()
 def send_file_status_email(title, message, addressee):
     email = EmailMessage(title, message, to=[addressee])
